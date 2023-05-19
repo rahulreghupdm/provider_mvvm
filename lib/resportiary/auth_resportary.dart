0815a84c2ff88a07f08fa89b89df86753c5d26a0
Rahul Reghu <108973886+rahulreghupdm@users.noreply.github.com>
2023-05-19 15:40:20 +0530

    inva

--- lib/resportiary/auth_resportary.dart
+++ lib/resportiary/auth_resportary.dart
@@ -3,12 +3,12 @@ import 'package:provider_mvvm_architecture/data/Network/networkApisServices.dart
 import 'package:provider_mvvm_architecture/res/app_url.dart';
 
 class AuthRepository {
-  BaseApiService _apiService = NetworkApiService();
+  BaseApiService apiService = NetworkApiService();
 
   Future<dynamic> loginApi(dynamic data) async {
     try {
       dynamic response =
-          await _apiService.getPostApiResponse(AppUrl.loginUrl, data);
+          await apiService.getPostApiResponse(AppUrl.loginUrl, data);
       return response;
     } catch (e) {
       rethrow;
